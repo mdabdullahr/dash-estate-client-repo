08ded3ce3f4410f523254f5eedbaedcd2c95df46
mbdullah <mdabdullah162005@gmail.com>
2025-07-10 13:49:37 +0600

    included initial setup

---
+++ src/Layouts/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <div>
+            Additional Dashboard
+        </div>
+    );
+};
+
+export default Dashboard;
---
+++ src/router/router.jsx
@@ -0,0 +1,8 @@
+import { createBrowserRouter } from "react-router";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div className="text-5xl text-red-500 font-bold">Hello World</div>,
+  },
+]);
